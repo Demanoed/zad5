c1899b9da491c6ca5c3d7c42fd7e12bc0a4f65b5
Дмитрий Трефилов <trefilowdem@mail.ru>
2019-06-15 00:45:31 +0500

    Add function CreateMatrix

--- Find max in lower triangle/Find max in lower triangle/Program.cs
+++ Find max in lower triangle/Find max in lower triangle/Program.cs
@@ -9,9 +9,34 @@ namespace Find_max_in_lower_triangle
 {
     class Program
     {
+        private static Random rnd = new Random();
+        private static double[,] matr;
+        public static void CreateMatrix(int x)
+        {
+            int k = Message.HowAdd();
+            if (k == 1)
+            {
+                for (int i = 0; i < x; i++)
+                    for (int j = 0; j < x; j++)
+                    {
+                        ColorMess.Cyan("\n Введите [" + i + "," + j + "] элемент матрицы: ");
+                        matr[i, j] = Input.Check(double.MinValue, double.MaxValue);
+                    }
+            }
+            else
+            {
+                for (int i = 0; i < x; i++)
+                    for (int j = 0; j < x; j++)
+                        matr[i, j] = rnd.Next(-100, 100);
+            }
+        }
         static void Main()
         {
-
+            ColorMess.Yellow("\n Введите количество строк и столбцов (одно число в диапазоне от 2 до 10): ");
+            int x = Input.Check(2, 10);
+            matr = new double[x, x];
+            CreateMatrix(x);
+            Console.Clear();
         }
     }
 }
